63abdcdd00192c132d56071fd95e6c980b66e561
Lucas Leperlier <lucas.leperlier@gmail.com>
2023-01-20 18:07:55 +0100

    update env

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'admin'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -67,9 +67,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'HelpDesk_g24hd2'), // HelpDesk_g24hd2_backup
-            'username' => env('DB_USERNAME', 'g24hd2'),
-            'password' => env('DB_PASSWORD', 'gjUZyU'),
+            'database' => env('DB_DATABASE', 'HelpDesk'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'admin'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -84,7 +84,7 @@ return [
             'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'admin'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -122,7 +122,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- resources/js/Pages/login.vue
+++ resources/js/Pages/login.vue
@@ -2,29 +2,56 @@
     <div>
         <div class="h-screen m-auto flex flex-col justify-center max-w-xs">
             <div class="w-full flex justify-center flex-row">
-                <img class="h-32" src="pictures/logo.png" alt="Logo HelpDesk">
-            </div> 
+                <img class="h-32" src="pictures/logo.png" alt="Logo HelpDesk" />
+            </div>
             <!-- bg-white -->
-            <form @submit.prevent="form.post($route('login'))" class="bg-gray-50 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-
+            <form
+                @submit.prevent="form.post($route('login'))"
+                class="bg-gray-50 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            >
                 <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    <label
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                        for="username"
+                    >
                         Utilisateur
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" v-model="form.email" placeholder="Mail">
-                    <b v-if="form.errors.email" class="text-red-500">{{ form.errors.email }}</b>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="email"
+                        v-model="form.email"
+                        placeholder="Mail"
+                    />
+                    <b v-if="form.errors.email" class="text-red-500">{{
+                        form.errors.email
+                    }}</b>
                 </div>
 
                 <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    <label
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                        for="password"
+                    >
                         Mot de passe
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" v-model="form.password" placeholder="*********">
-                    <b v-if="form.errors.password" class="text-red-500">{{ form.errors.password }}</b>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        v-model="form.password"
+                        placeholder="*********"
+                    />
+                    <b v-if="form.errors.password" class="text-red-500">{{
+                        form.errors.password
+                    }}</b>
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
                         Connexion
                     </button>
                 </div>
@@ -42,20 +69,17 @@
     </div>
 </template>
 
-
-<script>    
-
+<script>
 export default {
-    name:"Login",
-    components: {
-    },
-    data(){
-        return{
+    name: "Login",
+    components: {},
+    data() {
+        return {
             form: this.$inertia.form({
                 email: "",
-                password: ""
-            })
-        }
-    }
-}
+                password: "",
+            }),
+        };
+    },
+};
 </script>
